190f43b5c559d0bdbe3b996c54ded5ccc98426b3
Gisele Gomes <gisele.iron@hotmail.com>
2021-02-22 22:06:14 -0300

    Projetos

---
+++ DEV0102/DEV0102/Utils/Suporte.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace DEV0102.Utils
+{
+    public class Suporte
+    {
+        public void EnviarEmail(string Assunto, string emailDestinatario, string corpoEmail)
+        {
+            
+            MailMessage objmail = new MailMessage();
+            objmail.To.Add(emailDestinatario);
+            objmail.From = new MailAddress("gisele.iron@gmail.com");
+            objmail.Body = corpoEmail;
+            objmail.Subject = Assunto;
+
+            SmtpClient objsmtp = new SmtpClient();
+            objsmtp.Host = "smtp.gmail.com";
+            objsmtp.Port = 587;
+            objsmtp.UseDefaultCredentials = false;
+            objsmtp.Credentials = new NetworkCredential("gisele.iron@gmail.com", "fernando140110");
+            objsmtp.EnableSsl = true;
+            objsmtp.Send(objmail);
+        }
+    }
+}
